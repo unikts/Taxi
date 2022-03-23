479b849c765f36e5bfdd57648341c4a8eae2349c
unikts <unik.t@students.amikom.ac.id>
2022-03-23 20:24:13 +0700

    Project Taxi

---
+++ Taxi.cs/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taxi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Membuat Objek Taxi
+            Taxi satu = new Taxi();
+            Taxi dua = new Taxi();
+
+            // Pengesetan Nilai Properties
+            satu.DriverName = "Dyo";
+            satu.OnDuty = true;
+            satu.NumPassenger = 10;
+
+            dua.DriverName = "Brian";
+            dua.OnDuty = false;
+            dua.NumPassenger = 0;
+
+            // Pemanggilan Method
+            satu.TaxiInfo();
+            satu.PickUpPassenger();
+            satu.DropOffPassenger();
+
+            dua.TaxiInfo();
+            dua.PickUpPassenger();
+            dua.DropOffPassenger();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Taxi.cs/Taxi.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taxi
+{
+    public class Taxi
+    {
+        // Properties
+        public string DriverName { get; set; }
+        public bool OnDuty { get; set; }
+        public int NumPassenger { get; set; }
+
+        // Method
+        public void TaxiInfo()
+        {
+            Console.WriteLine("|===================================|");
+            Console.WriteLine("|            Taxi Info              |");
+            Console.WriteLine("|===================================|");
+            Console.WriteLine("Driver Name          : {0}         ", DriverName);
+            if (OnDuty == true)
+            {
+                Console.WriteLine("On Duty              : Yes         ", OnDuty);
+            } else {
+                Console.WriteLine("On Duty              : No         ", OnDuty);
+            }
+            Console.WriteLine("Number of Passenger  : {0}         ", NumPassenger);
+        }
+        public void PickUpPassenger()
+        {
+            if (OnDuty == true)
+            {
+                Console.WriteLine("{0} picking up passengers", DriverName, OnDuty);
+            } else
+            {
+                Console.WriteLine("{0} doesn't pick up passengers", DriverName, OnDuty);
+            }
+        }
+        public void DropOffPassenger()
+        {
+            if (OnDuty == true)
+            {
+                Console.WriteLine("{0} finished taking passengers\n", DriverName, OnDuty);
+            } else
+            {
+                Console.WriteLine("{0} doesn't finish taking passengers\n", DriverName, OnDuty);
+            }
+        }   
+    }
+}
